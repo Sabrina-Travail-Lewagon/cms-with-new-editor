22d5597e5409ef9655414903fbb0949d68b72661
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2024-01-24 09:46:37 +0100

    ajout routes articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/new'
-  get 'articles/edit'
   devise_for :users
   root to: "pages#home"
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
