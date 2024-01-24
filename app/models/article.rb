a92765ed07ec81a752643b78100318e017db7e46
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2024-01-24 09:45:38 +0100

    modif model article et user, ajout Bootstrap

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :user
+  validates :user, presence: true
+	  # Un article doit avoir un titre
+	  validates :title, presence: true, length: { minimum: 5 }
+	  # Ajout activeStorage aux articles pour pouvoir faciliter
+	  # has_many_attached :images, service: :local, dependent: :purge
+  has_many_attached :images, dependent: :purge
 end
